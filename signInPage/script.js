2f0d40714b4b41653a8bc77d29e644935f79fc9e
Abhishek7356 <ksabhishek32@gmail.com>
2023-09-06 14:46:38 +0530

    Made signup page more interactive

--- signInPage/script.js
+++ signInPage/script.js
@@ -1,20 +1,26 @@
 
 
-function signin(){
+function signin() {
     if (loginAcno.value == '' || loginPassword.value == '') {
         alert('please Enter details correctly');
-    } else{
-        if(loginAcno.value in localStorage){
+    } else {
+        if (loginAcno.value in localStorage) {
             let acnoDetails = JSON.parse(localStorage.getItem(loginAcno.value));
             console.log(acnoDetails);
-            if(loginPassword.value == acnoDetails.userPassword){
+            document.getElementById('incorrect2').style.display = 'none';
+            document.getElementById('loginAcno').style.borderBottom = '2px solid gray';
+            if (loginPassword.value == acnoDetails.userPassword) {
                 alert('Login Success');
                 location = '../homePage';
-            }else{
-                alert('incorrect password');
+                document.getElementById('incorrect').style.display = 'none';
+                document.getElementById('loginPassword').style.borderBottom = '2px solid gray';
+            } else {
+                document.getElementById('incorrect').style.display = 'block';
+                document.getElementById('loginPassword').style.borderBottom = '2px solid red';
             }
-        }else{
-            alert('User not found')
+        } else {
+            document.getElementById('incorrect2').style.display = 'block';
+            document.getElementById('loginAcno').style.borderBottom = '2px solid red';
         }
     }
 }
